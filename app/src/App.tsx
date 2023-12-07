bd6bc0524a5df2198c4cdc4a0073c122147975f7
mizore-fu <mhxxname0782@gmail.com>
2023-12-07 21:14:45 +0900

    アプリのUIを大まかに作成

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <div>
+        計測時間
+        <input type="number" defaultValue={0} />分
+        <input type="number" defaultValue={0} />秒
+      </div>
+      <p>残り時間 0秒</p>
+      <button>スタート</button>
+      <button>一時停止</button>
+      <button>リセット</button>
+    </div>
+  );
 }
 
 export default App;
